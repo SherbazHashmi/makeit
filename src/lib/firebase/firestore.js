11802ac3d00c4579fe0dffd6dc0ec0ce3595e8ed
Sherbaz Hashmi <sherbaz.hashmi@gmail.com>
2024-06-23 23:56:25 +1000

    added restaurants

--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -42,11 +42,36 @@ export async function addReviewToRestaurant(db, restaurantId, review) {
 }
 
 function applyQueryFilters(q, { category, city, price, sort }) {
-	return;
+	if (category) {
+					q = query(q, where("category", "==", category));
+	}
+	if (city) {
+					q = query(q, where("city", "==", city));
+	}
+	if (price) {
+					q = query(q, where("price", "==", price.length));
+	}
+	if (sort === "Rating" || !sort) {
+					q = query(q, orderBy("avgRating", "desc"));
+	} else if (sort === "Review") {
+					q = query(q, orderBy("numRatings", "desc"));
+	}
+	return q;
 }
 
 export async function getRestaurants(db = db, filters = {}) {
-	return [];
+	let q = query(collection(db, "restaurants"));
+
+	q = applyQueryFilters(q, filters);
+	const results = await getDocs(q);
+	return results.docs.map(doc => {
+					return {
+									id: doc.id,
+									...doc.data(),
+									// Only plain objects can be passed to Client Components from Server Components
+									timestamp: doc.data().timestamp.toDate(),
+					};
+	});
 }
 
 export function getRestaurantsSnapshot(cb, filters = {}) {
@@ -131,6 +156,7 @@ export async function addFakeRestaurantsAndReviews() {
 					ratingData
 				);
 			}
+			debugger;
 		} catch (e) {
 			console.log("There was an error adding the document");
 			console.error("Error adding document: ", e);
